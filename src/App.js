e4b0a39ca74d01ba5ee18d561d6d46ec537fb6aa
Rosemary Hogarth <rohogarth@gmail.com>
2024-09-09 13:51:13 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import Dictionary from "./Dictionary";
+import "./Dictionary.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     <h1>Hello there</h1>
-
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>
+        Coded by Rosemary Hogarth and open-sourced on GitHub
+      </footer>
     </div>
   );
 }
-
-export default App;
