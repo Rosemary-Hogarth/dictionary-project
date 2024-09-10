75acb6e00977030c10d5cb88d5d443b987226f5f
Rosemary Hogarth <rohogarth@gmail.com>
2024-09-10 10:54:01 +0200

    Added synonyms

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning";
 
+
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <p>[ {props.results.phonetic} ]</p>
+        <p>/{props.results.phonetic}/</p>
         {props.results.meanings.map(function(meaning, index) {
           return <div key={index}>
             <Meaning meaning={meaning} />
